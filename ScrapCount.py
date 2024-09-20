b5ba8781af1534cb638c162dd402e3f3c4e88d40
Nathan Lobo <lobonathan2209@gmail.com>
2024-09-20 20:23:29 +0530

    added press enter to exit terminal feature

--- ScrapCount.py
+++ ScrapCount.py
@@ -2,4 +2,4 @@ from myModule import getScrapCount
 scrapCount = getScrapCount()
 print(f'User DataPushed Count: {scrapCount[0]}')
 print(f'Profiles Pushed Count: {scrapCount[1]}')
-# input('Press Enter to exit')
+input('Press Enter to exit')
